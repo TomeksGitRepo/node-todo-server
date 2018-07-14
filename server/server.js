de37b4b776249299cf468e3852907c6fec9d2551
tom <tomek7sa@gmail.com>
2018-07-14 09:07:10 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const _ = require("lodash");
 let { mongoose } = require("./db/mongoose");
 let { Todo } = require("./models/todo");
 let { User } = require("./models/user");
-const {authenticate} = require('./middleware/authenticate');
+const { authenticate } = require("./middleware/authenticate");
+const bcrypt = require('bcryptjs');
 
 var app = express();
 const port = process.env.PORT;
@@ -117,25 +118,39 @@ app.patch("/todos/:id", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-  let body = _.pick(req.body, ['email', 'password']);
+  let body = _.pick(req.body, ["email", "password"]);
   let user = new User(body);
 
-  user.save().then(
-    () => {
+  user
+    .save()
+    .then(() => {
       return user.generateAuthToken();
-    }
-  ).then(token => {
-    res.header('x-auth', token).send(user);
-  }).catch(e => {
-    res.status(400).send(e);
-})
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 
+app.post("/users/login", (req, res) => {
+  let body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password).then(user => {
+      user.generateAuthToken().then((token) => {
+        res.header("x-auth", token).send(user);
+      });
+  }).catch(e => {
+    res.status(400).send();
+  })
 
 
-app.get('/users/me', authenticate, (req, res) => {
+});
+
+app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
-})
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
